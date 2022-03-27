896dcd6cb9f39f2750c720fb6b6523c6b069b877
Petri Miikki <miikinpetri@gmail.com>
2022-03-27 16:47:06 +0300

    Define theme palette that is used for mui

--- kalastaja.herrat/client/src/App.tsx
+++ kalastaja.herrat/client/src/App.tsx
@@ -3,7 +3,17 @@ import Router from './Router';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 export default function App() {
   return (
